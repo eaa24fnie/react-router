8dcbb223645700a82b635efab8d3c40e5ddd60d4
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-08-28 15:13:34 +0200

    FINAL IGEN

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 // main.jsx or App.jsximport React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/react-router/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
