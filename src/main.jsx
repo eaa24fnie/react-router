6422d352094aab6d3ee852106b1b97169f30f8b2
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-08-28 15:12:28 +0200

    fuck

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/react-router/"}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
