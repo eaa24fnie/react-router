66fb4fb4e1801f7ee6050b0d4585b18789b6c81c
eaa24fnie <eaa24fnie@students.eaaa.dk>
2025-08-28 14:59:21 +0200

    FINAL

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
